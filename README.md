d4ad8af01a236e436386dd862025cd49d34178be
Sean Soper <sean.soper@gmail.com>
2015-12-26 13:12:14 -0500

    remove debug stmts, add to README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # SwiftExec
 
 Cross-platform library that lets you execute a shell command from Swift and capture its output.
+
+## Use
+
+Add to your Package.swift.
+
+```swift
+    .Package(url: "https://github.com/ssoper/SwiftExec.git", majorVersion: 0, minor: 1)
+```
+
+Then use in your app.
+
+```swift
+import SwiftExec
+let output = swiftExec("/bin/echo", "relax", "we", "need", "some", "information", "first")
+```
--- Sources/SwiftExec.swift
+++ Sources/SwiftExec.swift
@@ -7,14 +7,11 @@
 import Foundation
 
 public func swiftExec(cmd: String, cmdArgs: String...) -> String {
-    print("STEP 1")
 
     #if os(Linux)
         var output = ""
         let joinedArgs = cmdArgs.joinWithSeparator(" ")
         let fullCmd = "\(cmd) \(joinedArgs)"
-        print("executing")
-        print(fullCmd)
         let fp = popen(fullCmd, "r")
         var buf = Array<CChar>(count: 128, repeatedValue: 0)
 
@@ -29,11 +26,11 @@ public func swiftExec(cmd: String, cmdArgs: String...) -> String {
         let task = NSTask()
         task.launchPath = cmd
         task.arguments = cmdArgs
-        print("there2")
+
         let pipe = NSPipe()
         task.standardOutput = pipe
         task.launch()
-        print("good3")
+
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: NSUTF8StringEncoding)
         return output!
